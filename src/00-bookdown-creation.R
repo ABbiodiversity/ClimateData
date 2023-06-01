5a2c9b1b7b72a03730f444249416dfb01b98d2f7
beallen <ballen@ualberta.ca>
2023-06-01 11:27:18 -0600

    Add bookdown and climate analysis code

---
+++ src/00-bookdown-creation.R
@@ -0,0 +1,17 @@
+library(bookdown)
+library(dplyr)
+library(kableExtra)
+
+# Bookdown resources https://bookdown.org/yihui/bookdown/github.html
+
+# Confirms the nojekyll is created
+file.create(paste0(getwd(), "/docs/.nojekyll"))
+
+# Render bookdown
+bookdown::render_book(input = "bookdown/", 
+                      output_format = "bookdown::gitbook",
+                      output_dir = paste0(getwd(), "/docs/"))
+
+
+
+
