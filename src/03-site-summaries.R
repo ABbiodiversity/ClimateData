83b9771f4b241110007af2c86a60ed1a99d5f87c
Brandon Allen <ballen@ualberta.ca>
2024-01-25 08:29:16 -0700

    Corrected aru/camera summaries

--- src/03-site-summaries.R
+++ src/03-site-summaries.R
@@ -1,10 +1,10 @@
 #
 # Title: Extraction of the climate data from the Climate NA software (Site summaries)
 # Created: December 4th, 2023
-# Last Updated: December 12th, 2023
+# Last Updated: January 20th, 2024
 # Author: Brandon Allen
 # Objectives: Extract site level summaries for the climate data
-# Keywords: Notes, Initialization, On grid Sites, Off grid Sites, Quadrants, ESID Sites, Camera/ARU Sites, Wetland Sites
+# Keywords: Notes, Initialization, On grid Sites, Off grid Sites, Quadrants, NEED TO DO ESID Sites, Camera/ARU Sites, Wetland Sites
 #
 
 ######### The latitude, longitude, and elevation information can't be stored permanently, so once extractions are
@@ -22,6 +22,7 @@ gc()
 # Load libraries
 library(ClimateNAr)
 library(reticulate)
+library(sf)
 
 # Initialize arcpy
 py_discover_config() # We need version 3.9
@@ -285,6 +286,16 @@ file.remove(files.remove)
 rm(list=ls())
 gc()
 
+# Stitch together all sites
+load("results/sites/off-grid-sites_2023.Rdata")
+climate.off <- climate.1ha
+load("results/sites/on-grid-sites_2023.Rdata")
+climate.1ha <- rbind(climate.1ha, climate.off)
+save(climate.1ha, file = "results/sites/terrestrial-sites_2023.Rdata")
+
+rm(list=ls())
+gc()
+
 #############
 # Quadrants # 
 #############~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -399,6 +410,296 @@ colnames(climate.qa)[1] <- "Site_Year_Quadrant"
 
 # Save
 save(climate.qa, file = "results/sites/terrestrial-quadrants_2023.Rdata")
+
+# Delete everything in the temporary folder
+files.remove <- list.files("D:/abmi/ClimateData/data/base/gis/temp/", full.names = TRUE)
+file.remove(files.remove)
+
+rm(list=ls())
+gc()
+
+##############
+# EISD Sites # 
+##############~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# Load the site paths, this file must be maintained in the 
+# "beta" folder and never uploaded to GitHub
+site.path <- read.csv("beta/site-paths_2023.csv")
+
+# Define the site path
+site.path <- paste0(site.path$Database[5], site.path$Feature[5])
+
+# Load arcpy
+arcpy <- import('arcpy') 
+
+# Define parallel processing factor
+arcpy$env$parallelProcessingFactor <- "100%"
+
+# Export the sites
+arcpy$ExportFeatures_conversion(in_features = site.path, 
+                                out_features = "D:/abmi/ClimateData/data/base/gis/temp/sites.shp")
+
+# Extract the DEM point values
+arcpy$sa$ExtractValuesToPoints(in_point_features = "D:/abmi/ClimateData/data/base/gis/temp/sites.shp", 
+                               in_raster = "D:/abmi/ClimateData/data/base/gis/dem/elevation_nasadem_30m.tif", 
+                               out_point_features = "D:/abmi/ClimateData/data/base/gis/temp/sites_dem.shp")
+
+# Extract the pAspen points
+arcpy$sa$ExtractValuesToPoints(in_point_features = "D:/abmi/ClimateData/data/base/gis/temp/sites.shp", 
+                               in_raster = "D:/abmi/ClimateData/data/base/gis/paspen/ASPEN9706.tif", 
+                               out_point_features = "D:/abmi/ClimateData/data/base/gis/temp/sites_paspen.shp")
+
+# Export the tables
+arcpy$ExportTable_conversion(in_table = "D:/abmi/ClimateData/data/base/gis/temp/sites_dem.shp", 
+                             out_table = "D:/abmi/ClimateData/data/base/gis/temp/sites_dem.csv")
+
+arcpy$ExportTable_conversion(in_table = "D:/abmi/ClimateData/data/base/gis/temp/sites_paspen.shp", 
+                             out_table = "D:/abmi/ClimateData/data/base/gis/temp/sites_paspen.csv")
+
+# Extract the information from ClimateNA
+# Standardize the output tables
+input.data <- read.csv("D:/abmi/ClimateData/data/base/gis/temp/sites_dem.csv")
+
+# Determine which columns identify year information
+year.col <- paste0("yr_", c(seq(2003,2019, 1), 2021, 2022, 2023))
+site.id <- data.frame(Site = NA,
+                      id1 = NA,
+                      id2 = NA,
+                      lat = NA,
+                      long = NA,
+                      elev = NA)
+
+for(col.id in year.col) {
+  
+  temp.data <- input.data[!(input.data[, col.id] %in% "N"), ]
+  
+  if(nrow(temp.data) == 0) next
+  
+  temp.data <- data.frame(Site = temp.data$ABMI,
+                          id1 = paste0(temp.data$ABMI_ID_Wi, "_", gsub("yr_", "", col.id)),
+                          id2 = paste0(temp.data$ABMI_ID_Wi, "_", gsub("yr_", "", col.id)),
+                          lat = temp.data$Latitude,
+                          long = temp.data$Longitude,
+                          elev = temp.data$RASTERVALU)
+  
+  site.id  <- rbind(site.id, temp.data)
+  rm(temp.data)
+  
+}
+
+site.id <- site.id[-1, ]
+
+# Save the results in the appropriate format appropriate input file
+write.csv(site.id[, -1], file = "D:/abmi/ClimateData/data/base/gis/temp/sites_cleaned.csv", row.names = FALSE)
+
+# Call the ClimateNA API to calculate the relevant climate variables
+# NOTE: We need to use the double backslash for the executable to read the information correctly
+ClimateNA_cmdLine(exe = "ClimateNA_v7.40.exe", 
+                  wkDir = "D:\\climateNA\\ClimateNA\\", 
+                  period = "Normal_1991_2020.nrm", 
+                  MSY = "Y", 
+                  inputFile = "D:\\abmi\\ClimateData\\data\\base\\gis\\temp\\sites_cleaned.csv", 
+                  outputFile = "D:\\abmi\\ClimateData\\data\\base\\gis\\temp\\sites_climate.csv")
+
+# Format the paspen information
+input.data <- read.csv("D:/abmi/ClimateData/data/base/gis/temp/sites_paspen.csv")
+
+# Determine which columns identify year information
+year.col <- paste0("yr_", c(seq(2003,2019, 1), 2021, 2022, 2023))
+site.id <- data.frame(Site = NA,
+                      id1 = NA,
+                      id2 = NA,
+                      lat = NA,
+                      long = NA,
+                      pAspen = NA)
+
+for(col.id in year.col) {
+  
+  temp.data <- input.data[!(input.data[, col.id] %in% "N"), ]
+  
+  if(nrow(temp.data) == 0) next
+  
+  temp.data <- data.frame(Site = temp.data$ABMI,
+                          id1 = paste0(temp.data$ABMI_ID_Wi, "_", gsub("yr_", "", col.id)),
+                          id2 = paste0(temp.data$ABMI_ID_Wi, "_", gsub("yr_", "", col.id)),
+                          lat = temp.data$Latitude,
+                          long = temp.data$Longitude,
+                          pAspen = temp.data$RASTERVALU)
+  
+  site.id  <- rbind(site.id, temp.data)
+  rm(temp.data)
+  
+}
+
+site.id <- site.id[-1, ]
+
+# Add the public site information
+public.coordinates <- read.csv("data/lookup/public-coordiantes.csv")
+public.coordinates <- public.coordinates[, c("SITE_ID", "PUBLIC_LATTITUDE", "PUBLIC_LONGITUDE",
+                                             "ELEVATION", "NATURAL_REGIONS", "NATURAL_SUBREGIONS")]
+colnames(public.coordinates) <- c("Site", "Latitude", "Longitude", "Elevation", 
+                                  "NrName", "NsrName")
+
+# There are a few missing sites names, so we need to add them
+site.id$Site[site.id$id1 %in% c("ST-430-1_2023", "ST-430-2_2023", "ST-430-3_2023")] <- 430
+site.id$Site[site.id$id1 %in% c("ST-1135-1_2023", "ST-1135-2_2023")] <- 1135
+site.id$Site[site.id$id1 %in% c("ST-1169-1_2023")] <- 1169
+
+site.id <- merge.data.frame(public.coordinates, site.id[, c("Site", "id1", "pAspen")], by = "Site")
+site.id <- site.id[, -1]
+
+# Stitch together the paspen and climate information
+climate.data <- read.csv("D:/abmi/ClimateData/data/base/gis/temp/sites_climate.csv")
+climate.data <- climate.data[, !(colnames(climate.data) %in% c("id2", "Latitude", "Longitude",
+                                                               "Elevation", "MAR"))]
+
+climate.1ha <- merge.data.frame(site.id, climate.data, by = "id1")
+colnames(climate.1ha)[1] <- "Site_Year"
+
+# Save
+save(climate.1ha, file = "results/sites/on-grid-sites_2023.Rdata")
+
+# Delete everything in the temporary folder
+files.remove <- list.files("D:/abmi/ClimateData/data/base/gis/temp/", full.names = TRUE)
+file.remove(files.remove)
+
+rm(list=ls())
+gc()
+
+##############
+# Camera/ARU # Mess of stuff. No public coordinates, both cam and aru info (done twice, one for camera one for aru).
+##############~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# Load the site paths, this file must be maintained in the 
+# "beta" folder and never uploaded to GitHub
+site.path <- read.csv("beta/site-paths_2023.csv")
+
+# Define the site path
+site.path <- paste0(site.path$Database[3], site.path$Feature[3])
+
+# Load arcpy
+arcpy <- import('arcpy') 
+
+# Define parallel processing factor
+arcpy$env$parallelProcessingFactor <- "100%"
+
+# Export the sites
+arcpy$ExportFeatures_conversion(in_features = site.path, 
+                                out_features = "D:/abmi/ClimateData/data/base/gis/temp/sites.shp")
+
+# Extract the DEM point values
+arcpy$sa$ExtractValuesToPoints(in_point_features = "D:/abmi/ClimateData/data/base/gis/temp/sites.shp", 
+                               in_raster = "D:/abmi/ClimateData/data/base/gis/dem/elevation_nasadem_30m.tif", 
+                               out_point_features = "D:/abmi/ClimateData/data/base/gis/temp/sites_dem.shp")
+
+# Extract the pAspen points
+arcpy$sa$ExtractValuesToPoints(in_point_features = "D:/abmi/ClimateData/data/base/gis/temp/sites.shp", 
+                               in_raster = "D:/abmi/ClimateData/data/base/gis/paspen/ASPEN9706.tif", 
+                               out_point_features = "D:/abmi/ClimateData/data/base/gis/temp/sites_paspen.shp")
+
+# Find the nearest distance of the sites to public locations
+arcpy$GenerateNearTable_analysis(in_features = "D:/abmi/ClimateData/data/base/gis/temp/sites.shp", 
+                                 near_features = "D:/spatial/ABMI.gdb/ABMI_3x7", 
+                                 out_table = "D:/abmi/ClimateData/data/base/gis/temp/sites_public.csv",
+                                 method = "GEODESIC")
+
+# Export the tables
+arcpy$ExportTable_conversion(in_table = "D:/abmi/ClimateData/data/base/gis/temp/sites_dem.shp", 
+                             out_table = "D:/abmi/ClimateData/data/base/gis/temp/sites_dem.csv")
+
+arcpy$ExportTable_conversion(in_table = "D:/abmi/ClimateData/data/base/gis/temp/sites_paspen.shp", 
+                             out_table = "D:/abmi/ClimateData/data/base/gis/temp/sites_paspen.csv")
+
+# Extract the information from ClimateNA
+# Standardize the output tables
+input.data <- read.csv("D:/abmi/ClimateData/data/base/gis/temp/sites_dem.csv")
+input.data$Site_Year <- paste0(input.data$Site_ID, "_", input.data$Year)
+
+# Format the kgrid to be the appropriate input file
+input.data <- input.data[, c("Site_Year", "Site_Year", "Latitude",  "Longitude", "RASTERVALU")]
+colnames(input.data) <- c("id1", "id2", "lat", "long", "elev")
+
+write.csv(input.data, file = "D:/abmi/ClimateData/data/base/gis/temp/sites_cleaned.csv", row.names = FALSE)
+
+# Call the ClimateNA API to calculate the relevant climate variables
+# NOTE: We need to use the double backslash for the executable to read the information correctly
+ClimateNA_cmdLine(exe = "ClimateNA_v7.40.exe", 
+                  wkDir = "D:\\climateNA\\ClimateNA\\", 
+                  period = "Normal_1991_2020.nrm", 
+                  MSY = "Y", 
+                  inputFile = "D:\\abmi\\ClimateData\\data\\base\\gis\\temp\\sites_cleaned.csv", 
+                  outputFile = "D:\\abmi\\ClimateData\\data\\base\\gis\\temp\\sites_climate.csv")
+
+# Take site locations and combine with the extracted public location
+input.data <- read_sf(dsn = "D:/abmi/ClimateData/data/base/gis/temp/sites.shp")
+input.data <- as.data.frame(input.data)
+input.data$IN_FID <- as.numeric(rownames(input.data)) - 1
+input.data$Site_Year <- paste0(input.data$Site_ID, "_", input.data$Year)
+
+public.site <- read.csv("D:/abmi/ClimateData/data/base/gis/temp/sites_public.csv")
+input.data <- merge.data.frame(input.data[, c("IN_FID", "Site_Year")], 
+                               public.site[, c("IN_FID", "NEAR_FID")], by = "IN_FID")
+
+# Merge the NEAR_ID field with the original file from the 3x7s
+public.gis <- read_sf(dsn = "D:/spatial/ABMI.gdb/", layer = "ABMI_3x7")
+public.gis <- as.data.frame(public.gis)
+colnames(public.gis)[1] <- "NEAR_FID"
+input.data <- merge.data.frame(input.data, 
+                               public.gis[, c("NEAR_FID", "ABMI")], by = "NEAR_FID")
+
+# Now that we have the nearest site, combine with the public lookup table.
+public.data <- read.csv("D:/abmi/ClimateData/data/lookup/public-coordiantes.csv")
+colnames(public.data)[1] <- "ABMI"
+
+public.data <- merge.data.frame(input.data[, c("ABMI", "Site_Year")], public.data, by = "ABMI")
+
+# Format the climate data
+climate.in <- read.csv("D:/abmi/ClimateData/data/base/gis/temp/sites_climate.csv")
+colnames(climate.in)[1] <- "Site_Year"
+site.data <- merge.data.frame(climate.in, public.data, by = "Site_Year")
+
+# Format the paspen information
+input.data <- read.csv("D:/abmi/ClimateData/data/base/gis/temp/sites_paspen.csv")
+
+# Determine which columns identify year information
+input.data <- data.frame(Site_Year = paste0(input.data$Site_ID, "_", input.data$Year),
+                         deployment = input.data$deployment,
+                         pAspen = input.data$RASTERVALU)
+site.data <- merge.data.frame(site.data, input.data, by = "Site_Year")
+
+# Mask the latitude, longitude, and elevation information
+site.data$Latitude <- 0
+site.data$Longitude <- 0
+site.data$Elevation <- 0
+
+site.data$Latitude <- site.data$PUBLIC_LATTITUDE
+site.data$Longitude <- site.data$PUBLIC_LONGITUDE
+site.data$Elevation <- site.data$ELEVATION
+
+# Correct column names
+colnames(site.data)[colnames(site.data) %in% "NATURAL_REGIONS"] <- "NrName"
+colnames(site.data)[colnames(site.data) %in% "NATURAL_SUBREGIONS"] <- "NsrName"
+
+# Pull out the camera deployments
+camera.climate <- site.data[site.data$deployment %in% c("BOTH", "CAM"), ]
+aru.climate <- site.data[site.data$deployment %in% c("BOTH", "ARU"), ]
+
+# Format the columns
+camera.climate <- camera.climate[, c("Site_Year", "Latitude", "Longitude", "Elevation", "NrName",
+                           "NsrName", "pAspen", "MAT", "MWMT", "MCMT", "TD", "MAP",
+                           "MSP", "AHM", "SHM", "DD_0", "DD5", "DD_18", "DD18", "NFFD",
+                           "bFFP", "eFFP", "FFP", "PAS", "EMT", "EXT", "Eref", "CMD",
+                           "RH", "CMI", "DD1040")]
+
+aru.climate <- aru.climate[, c("Site_Year", "Latitude", "Longitude", "Elevation", "NrName",
+                               "NsrName", "pAspen", "MAT", "MWMT", "MCMT", "TD", "MAP",
+                               "MSP", "AHM", "SHM", "DD_0", "DD5", "DD_18", "DD18", "NFFD",
+                               "bFFP", "eFFP", "FFP", "PAS", "EMT", "EXT", "Eref", "CMD",
+                               "RH", "CMI", "DD1040")]
+
+# Save
+save(camera.climate, file = "results/sites/camera-sites_climate_2023.Rdata")
+save(aru.climate, file = "results/sites/aru-sites_climate_2023.Rdata")
 
 # Delete everything in the temporary folder
 files.remove <- list.files("D:/abmi/ClimateData/data/base/gis/temp/", full.names = TRUE)
